2ae8fd0f118908d573f2ee211904201f4689d253
restubumialfathar <restubumialfathar12@gmail.com>
2024-12-09 14:59:41 +0700

    dahlah

--- build/web/flutter_service_worker.js
+++ build/web/flutter_service_worker.js
@@ -103,7 +103,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "95dde51dd15668aa7837c271f1c2e251
 "canvaskit/skwasm.worker.js": "bfb704a6c714a75da9ef320991e88b03",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "383e55f7f3cce5be08fcf1f3881f585c",
-"flutter_bootstrap.js": "af4d868a7e534bb7b777ae157d1438ca",
+"flutter_bootstrap.js": "dc1f5eb7fc4b16578547c7f3285dd7fd",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
